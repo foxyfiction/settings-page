83f19625a929cc2e7ea1c1a77817d254713ccfb6
Alexandra Kondyurina <akondurina@hwdtech.com>
2017-10-13 03:03:10 +0600

    Create navigation bar component

---
+++ app/components/nav-bar.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'nav',
+  classNames: ['navbar', 'navbar-default'],
+  attributeBindings: ['role'],
+  role: 'navigation'
+});
