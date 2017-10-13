e11732a96de8d8e67bcfdd4f743ecf4e694bed9b
Alexandra Kondyurina <akondurina@hwdtech.com>
2017-10-13 19:27:27 +0600

    Add resetController hook to setting route

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -8,20 +8,18 @@ export default Ember.Route.extend({
     return this.get('store').findRecord('setting', 1);
   },
 
-  actions: {
-    willTransition() {
-      this.controller.setProperties({
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.setProperties({
         isResultMessageVisible: false,
         resultMessageTitle: null,
         resultMessage: null,
         resultMessageType: null,
       });
-      const model = this.controller.get('model');
-      if (model.get('hasDirtyAttributes')) {
-        model.rollbackAttributes();
-      }
-      return true;
-    },
+    }
+  },
+
+  actions: {
     error(error) {
       if (error) {
         this.get('notifications').error('Failed to load settings.', {
